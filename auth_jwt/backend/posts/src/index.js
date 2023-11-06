203d2ce0a44e1df8a79f77b0500f3b240299d151
Oleksandr Nykytin <keeper@ninydev.com>
2023-11-06 20:19:28 +0200

    Jwt MiddleWare

--- auth_jwt/backend/posts/src/index.js
+++ auth_jwt/backend/posts/src/index.js
@@ -3,6 +3,14 @@ let express = require('express');
 let app = express();
 
 
+let auth = require("./middlewares/jwtAuth").auth;
+
+app.get("/api/posts", auth, function (request, response)  {
+    return response.status(200).json({
+        "user": request.user
+    });
+} );
+
 
 // Запустим веб сервер
 app.listen(80, () => {
---
+++ auth_jwt/backend/posts/src/middlewares/jwtAuth.js
@@ -0,0 +1,34 @@
+jwt = require('jsonwebtoken');
+const JWT_KEY = process.env.JWT_KEY;
+
+exports.auth = function (request, response, next) {
+    // Если передали ключ - проверю его
+    if (request.headers.authorization) {
+        console.log(request.headers.authorization);
+
+        // Получите JWT из заголовка Authorization
+        const authHeader = request.headers.authorization;
+        const token = authHeader.replace('Bearer ', ''); // Удалите "Bearer " из заголовка
+
+        jwt.verify(
+            token,
+            process.env.JWT_KEY,
+            (err, jwtUser) => {
+                if (err) {
+                    console.log("Ошибка расшифровки");
+                    console.log(err);
+                    next();
+                } // если ошибка - просто пойду дальше
+
+                console.log("Восстановленные данные");
+                console.log(jwtUser);
+                // Добавим в запрос данные о пользователе
+                request.user = jwtUser;
+            }
+        )
+    }
+
+    // Пойти дальше
+    next();
+
+}
