e1b90bbc1ec17d8ccf5e6fe18a1704d83aa90112
Oleksandr Nykytin <keeper@ninydev.com>
2023-12-14 20:52:57 +0200

    Nginx Router

--- normal/src/public/index.php
+++ normal/src/public/index.php
@@ -1,10 +1,32 @@
 <?php
 require_once '../vendor/autoload.php';
+/*
+// Получаем QUERY_STRING из $_SERVER
+$queryString = isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : '';
 
-$controllerName = isset($_GET['controller']) ? $_GET['controller'] : 'Page';
+// Разбиваем QUERY_STRING по символу '/'
+$queryStringParts = explode('/', $queryString);
+
+// Получаем название контроллера и метода
+$controllerName = isset($queryStringParts[0]) ? ucfirst($queryStringParts[0]) . 'Controller' : 'DefaultController';
+$methodName = isset($queryStringParts[1]) ? $queryStringParts[1] : 'index';
+
+*/
+
+// Получаем QUERY_STRING из $_SERVER
+$queryString = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
+// Разбиваем QUERY_STRING по символу '/'
+$queryStringParts = explode('/', $queryString);
+
+var_dump($queryStringParts);
+
+$controllerName = isset($queryStringParts[1]) ? ucfirst($queryStringParts[1]) : 'Page';
 $controllerClassName = "\App\Controllers\\". $controllerName . 'Controller';
+$methodName = isset($queryStringParts[2]) ? $queryStringParts[2] : 'index';
 
-$methodName = isset($_GET['method']) ? $_GET['method'] : 'index';
+//$controllerName = isset($_GET['controller']) ? $_GET['controller'] : 'Page';
+//$controllerClassName = "\App\Controllers\\". $controllerName . 'Controller';
+//$methodName = isset($_GET['method']) ? $_GET['method'] : 'index';
 
 echo "<p> Try Find " . $controllerClassName . "::" . $methodName . "</p>";
 
